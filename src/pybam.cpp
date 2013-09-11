dbdfcb8bedc0d5151e7e83e9d55c4f613cc82b97
Andrew McPherson <andrew.mcpherson@gmail.com>
2013-09-10 17:16:22 -0700

    fixed major bug with updating pileup

--- src/pybam.cpp
+++ src/pybam.cpp
@@ -29,16 +29,99 @@ struct PileupQueue : PileupVisitor
 {
 	void Visit(const PileupPosition& pileupData)
 	{
-		Pileups.push(pileupData);
+		int ntData[5][6] = {{0}};
+		int ambiguous = 0;
+		
+		for (vector<PileupAlignment>::const_iterator pileupIter = pileupData.PileupAlignments.begin(); pileupIter != pileupData.PileupAlignments.end(); ++pileupIter)
+		{
+			const PileupAlignment& pa = (*pileupIter);
+			const BamAlignment& ba = pa.Alignment;
+			
+			if (pa.IsCurrentDeletion)
+			{
+				continue;
+			}
+			
+			char base = toupper(ba.QueryBases.at(pa.PositionInAlignment));
+			
+			if (base == 'N')
+			{
+				ambiguous++;
+				continue;
+			}
+			
+			int baseIdx;
+			switch (base)
+			{
+				case 'A': baseIdx = 0; break;
+				case 'C': baseIdx = 1; break;
+				case 'G': baseIdx = 2; break;
+				case 'T': baseIdx = 3; break;
+				default: throw runtime_error("unrecognized base " + string(1, base));
+			}
+			
+			// count
+			ntData[baseIdx][0]++;
+			ntData[4][0]++;
+			
+			// quality
+			ntData[baseIdx][1] += ba.Qualities.at(pa.PositionInAlignment);
+			ntData[4][1] += ba.Qualities.at(pa.PositionInAlignment);
+			
+			// mapping quality
+			ntData[baseIdx][2] += ba.MapQuality;
+			ntData[4][2] += ba.MapQuality;
+			
+			// distance
+			ntData[baseIdx][3] += (ba.IsReverseStrand()) ? ba.Length - pa.PositionInAlignment - 1 : pa.PositionInAlignment;
+			ntData[4][3] += (ba.IsReverseStrand()) ? ba.Length - pa.PositionInAlignment - 1 : pa.PositionInAlignment;
+			
+			// direction
+			ntData[baseIdx][3] += (ba.IsReverseStrand()) ? 1 : 0;
+			ntData[4][3] += (ba.IsReverseStrand()) ? 1 : 0;
+		}
+		
+		// Identify major base
+		int majorBaseIdx = 0;
+		for (int baseIdx = 0; baseIdx < 4; baseIdx++)
+		{
+			if (ntData[baseIdx][0] > ntData[majorBaseIdx][0])
+			{
+				majorBaseIdx = baseIdx;
+			}
+		}
+		
+		// Identify minor base, initialize to base that is not the major base
+		int minorBaseIdx = (majorBaseIdx + 1) % 4;
+		for (int baseIdx = 0; baseIdx < 4; baseIdx++)
+		{
+			if (ntData[baseIdx][0] > ntData[minorBaseIdx][0] && baseIdx != majorBaseIdx)
+			{
+				majorBaseIdx = baseIdx;
+			}
+		}
+		
+		// Interface is 1-based, bamtools is 0-based
+		int position = pileupData.Position + 1;
+		
+		Tuples.push(python::make_tuple(pileupData.RefId, position,
+								  python::make_tuple(ntData[0][0], ntData[0][1], ntData[0][2], ntData[0][3], ntData[0][4], ntData[0][5]),
+								  python::make_tuple(ntData[1][0], ntData[1][1], ntData[1][2], ntData[1][3], ntData[1][4], ntData[1][5]),
+								  python::make_tuple(ntData[2][0], ntData[2][1], ntData[2][2], ntData[2][3], ntData[2][4], ntData[2][5]),
+								  python::make_tuple(ntData[3][0], ntData[3][1], ntData[3][2], ntData[3][3], ntData[3][4], ntData[3][5]),
+								  python::make_tuple(ntData[4][0], ntData[4][1], ntData[4][2], ntData[4][3], ntData[4][4], ntData[4][5]),
+								  majorBaseIdx,
+								  minorBaseIdx,
+								  ambiguous));
 	}
 	
 	void Clear()
 	{
-		std::queue<PileupPosition> empty;
-		swap(Pileups, empty);
+		std::queue<python::tuple> empty;
+		swap(Tuples, empty);
 	}
 	
-	std::queue<PileupPosition> Pileups;
+	std::queue<python::tuple> Tuples;
 };
 
 class Pileup
@@ -87,19 +170,19 @@ public:
 		{
 			m_PileupEngine.AddAlignment(al);
 			
-			if (m_PileupQueue.Pileups.empty())
+			if (m_PileupQueue.Tuples.empty())
 			{
 				continue;
 			}
 			
 			// Remove positions before our target
-			while (!m_PileupQueue.Pileups.empty() && m_PileupQueue.Pileups.front().RefId == refID && m_PileupQueue.Pileups.front().Position < position)
+			while (!m_PileupQueue.Tuples.empty() && m_PileupQueue.Tuples.front()[0] == refID && m_PileupQueue.Tuples.front()[1] < position)
 			{
-				m_PileupQueue.Pileups.pop();
+				m_PileupQueue.Tuples.pop();
 			}
 			
 			// Check if we have hit or passed our target
-			if (!m_PileupQueue.Pileups.empty() && (m_PileupQueue.Pileups.front().RefId != refID || m_PileupQueue.Pileups.front().Position >= position))
+			if (!m_PileupQueue.Tuples.empty() && (m_PileupQueue.Tuples.front()[0] != refID || m_PileupQueue.Tuples.front()[1] >= position))
 			{
 				break;
 			}
@@ -109,104 +192,26 @@ public:
 	python::object Next()
 	{
 		BamAlignment al;
-		while (m_BamReader.GetNextAlignment(al) && m_PileupQueue.Pileups.empty())
+		while (m_BamReader.GetNextAlignment(al))
 		{
 			m_PileupEngine.AddAlignment(al);
+			
+			if (!m_PileupQueue.Tuples.empty())
+			{
+				break;
+			}
 		}
 		
-		if (!m_PileupQueue.Pileups.empty())
+		if (m_PileupQueue.Tuples.empty())
 		{
-			const PileupPosition& pileupData = m_PileupQueue.Pileups.front();
-			
-			int ntData[5][6] = {{0}};
-			int ambiguous = 0;
-			
-			for (vector<PileupAlignment>::const_iterator pileupIter = pileupData.PileupAlignments.begin(); pileupIter != pileupData.PileupAlignments.end(); ++pileupIter)
-			{
-				const PileupAlignment& pa = (*pileupIter);
-				const BamAlignment& ba = pa.Alignment;
-				
-				if (pa.IsCurrentDeletion)
-				{
-					continue;
-				}
-				
-				char base = toupper(ba.QueryBases.at(pa.PositionInAlignment));
-				
-				if (base == 'N')
-				{
-					ambiguous++;
-					continue;
-				}
-				
-				int baseIdx;
-				switch (base)
-				{
-					case 'A': baseIdx = 0; break;
-					case 'C': baseIdx = 1; break;
-					case 'G': baseIdx = 2; break;
-					case 'T': baseIdx = 3; break;
-					default: throw runtime_error("unrecognized base " + string(1, base));
-				}
-				
-				// count
-				ntData[baseIdx][0]++;
-				ntData[4][0]++;
-				
-				// quality
-				ntData[baseIdx][1] += ba.Qualities.at(pa.PositionInAlignment);
-				ntData[4][1] += ba.Qualities.at(pa.PositionInAlignment);
-				
-				// mapping quality
-				ntData[baseIdx][2] += ba.MapQuality;
-				ntData[4][2] += ba.MapQuality;
-				
-				// distance
-				ntData[baseIdx][3] += (ba.IsReverseStrand()) ? ba.Length - pa.PositionInAlignment - 1 : pa.PositionInAlignment;
-				ntData[4][3] += (ba.IsReverseStrand()) ? ba.Length - pa.PositionInAlignment - 1 : pa.PositionInAlignment;
-				
-				// direction
-				ntData[baseIdx][3] += (ba.IsReverseStrand()) ? 1 : 0;
-				ntData[4][3] += (ba.IsReverseStrand()) ? 1 : 0;
-			}
-			
-			// Identify major base
-			int majorBaseIdx = 0;
-			for (int baseIdx = 0; baseIdx < 4; baseIdx++)
-			{
-				if (ntData[baseIdx][0] > ntData[majorBaseIdx][0])
-				{
-					majorBaseIdx = baseIdx;
-				}
-			}
-			
-			// Identify minor base, initialize to base that is not the major base
-			int minorBaseIdx = (majorBaseIdx + 1) % 4;
-			for (int baseIdx = 0; baseIdx < 4; baseIdx++)
-			{
-				if (ntData[baseIdx][0] > ntData[minorBaseIdx][0] && baseIdx != majorBaseIdx)
-				{
-					majorBaseIdx = baseIdx;
-				}
-			}
-			
-			m_PileupQueue.Pileups.pop();
-			
-			// Interface is 1-based, bamtools is 0-based
-			int position = pileupData.Position + 1;
-			
-			return python::make_tuple(position,
-									  python::make_tuple(ntData[0][0], ntData[0][1], ntData[0][2], ntData[0][3], ntData[0][4], ntData[0][5]),
-									  python::make_tuple(ntData[1][0], ntData[1][1], ntData[1][2], ntData[1][3], ntData[1][4], ntData[1][5]),
-									  python::make_tuple(ntData[2][0], ntData[2][1], ntData[2][2], ntData[2][3], ntData[2][4], ntData[2][5]),
-									  python::make_tuple(ntData[3][0], ntData[3][1], ntData[3][2], ntData[3][3], ntData[3][4], ntData[3][5]),
-									  python::make_tuple(ntData[4][0], ntData[4][1], ntData[4][2], ntData[4][3], ntData[4][4], ntData[4][5]),
-									  majorBaseIdx,
-									  minorBaseIdx,
-									  ambiguous);
+			return python::object();
 		}
 		
-		return python::object();
+		python::tuple tpl;
+		swap(tpl, m_PileupQueue.Tuples.front());
+		m_PileupQueue.Tuples.pop();
+		
+		return tpl;
 	}
 	
 private:
